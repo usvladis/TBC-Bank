18ee0294fb1776591e137dbdf2e70215511c48c2
usvladis <usvladis@gmail.com>
2024-07-31 19:58:05 +1000

    Initial Commit

---
+++ TBC Bank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TBC Bank
+//
+//  Created by Владислав Усачев on 31.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
